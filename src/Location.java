dc25f264e37f05e4db78e22a1cdb48815bb5ed74
malinek <malinek.vojta@gmail.com>
2025-03-02 13:14:46 +0100

    Pridani trid world + Location + location trida udelana

---
+++ src/Location.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Location {
+    private int id;
+    private String name;
+    private ArrayList<Integer> neighbor ;
+
+    public Location(int id, String name, ArrayList<Integer> neighbor ) {
+        this.id = id;
+        this.name = name;
+        this.neighbor  = neighbor ;
+    }
+
+
+    public ArrayList<Integer> getNeighbor() {
+        return neighbor;
+    }
+}
