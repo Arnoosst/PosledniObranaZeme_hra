70cf86ab84a29e15fbdf86f67fba36d106a0067c
malinek <malinek.vojta@gmail.com>
2025-03-02 13:19:22 +0100

    Udelana metoda loadMap ve tride world

--- src/World.java
+++ src/World.java
@@ -1,2 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class World {
+    private static HashMap<Integer, Location> map = new HashMap<>();
+
+
+    public static void loadMap(String soubor) {
+        try (BufferedReader br = new BufferedReader(new FileReader(soubor))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(";");
+                int id = Integer.parseInt(parts[0]);
+                String nazev = parts[1];
+                ArrayList<Integer> neighbor = new ArrayList<>();
+                for (String s : parts[2].split(",")) {
+                    neighbor.add(Integer.parseInt(s));
+                }
+                map.put(id, new Location(id, nazev, neighbor));
+
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
