adeb2c860a11f96019458b1cf17efe8697b40c3f
malinek <malinek.vojta@gmail.com>
2025-03-18 21:13:36 +0100

    .

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -2,8 +2,5 @@ package Game;
 
 public class Main {
     public static void main(String[] args) {
-        World.loadMap("map.txt");
-        System.out.println(World.moveTo(2));
-        System.out.println(World.moveTo(3));
     }
 }
